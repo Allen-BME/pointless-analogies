67abf3f3a457e7be056fcc6fce5f2859e0f8e301
Jacob Guillemette <NinteMars@proton.me>
2024-11-20 20:35:53 -0500

    Update stack to include index lambda
    Updates the stack to include the index lambda and its associated
    ApiGateway
    
    Signed-off-by: Jacob Guillemette <NinteMars@proton.me>

--- pointless_analogies/pointless_analogies_stack.py
+++ pointless_analogies/pointless_analogies_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw
 )
 from constructs import Construct
 
@@ -10,7 +11,20 @@ class PointlessAnalogiesStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        test_fun = _lambda.Function(
+            self,
+            "Index",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            handler="index.lambda_handler",
+            code=_lambda.Code.from_asset("lambda/")
+        )
+
+        endpoint = apigw.LambdaRestApi(
+            self,
+            "ApiGwEndpoint",
+            handler=test_fun,
+            rest_api_name="IndexApi"
+        )
 
         # example resource
         # queue = sqs.Queue(
