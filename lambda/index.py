275f5595877c0012f98a3a86110bed255120a645
Jacob Guillemette <NinteMars@proton.me>
2024-11-24 23:16:09 -0500

    Add code to display each image in S3 bucket
    Adds code to connect to an S3 bucket, which grabs each object in the
    bucket, creates a url where its accessible, and adds each to the HTML
    source.
    
    Signed-off-by: Jacob Guillemette <NinteMars@proton.me>

--- lambda/index.py
+++ lambda/index.py
@@ -1,5 +1,20 @@
+import json
+import boto3
+import os
+from botocore.exceptions import ClientError
+
 def lambda_handler(event, context):
-    body = '''
+    s3_client = boto3.client('s3')
+    bucket_name: str = os.environ['BUCKET_NAME']
+    object_name: str = 'test.jpg'
+    s3_resource = boto3.resource('s3')
+    bucket = s3_resource.Bucket(bucket_name)
+
+    images = []
+    for obj in bucket.objects.all():
+        images.append(f"https://{bucket_name}.s3.amazonaws.com/{obj.key}")
+
+    body = f'''
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -8,7 +23,17 @@ def lambda_handler(event, context):
     </head>
     <body>
         <h1>Pointless Analogies</h1>
-        <p>Initial lambda function</p>
+        <h2>Images in S3 Bucket</h2>
+    '''
+
+    for image in images:
+        print(image)
+
+    for image in images:
+        body += f"<img src='{image}' alt='S3 Image' style='width:300px;height:auto;'/><br>\n"
+
+    body += '''
+    </body>
     </html>
     '''
 
