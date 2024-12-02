786848d1cc8d9dab76fe763f1d99311040ea117e
Christian Ambrosini <Christian_Ambrosini@student.uml.edu>
2024-12-02 13:09:15 -0500

    category changes

--- lambda/categories.py
+++ lambda/categories.py
@@ -1,6 +1,12 @@
 import random
+import boto3
+import json
+import os
+from botocore.exceptions import ClientError
 
-categories = ["chair", "hat", "boat", "shoe", "wig", "hair tie", "apple", "toothbrush",
+def lambda_handler(event, context):
+    categories = ["chair", "hat", "boat", "shoe", "wig", "hair tie", "apple", "toothbrush",
               "fork", "shirt", "belt", "table", "bat", "car", "pen", "bicycle",
               "ice cube tray", "knife", "purse", "cat"]
-result = random.choice(categories)
+    result = random.choice(categories)
+    return result
