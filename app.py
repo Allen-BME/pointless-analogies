c0332f8a9058c450b3046e0ba74f5b79728162a9
Jacob Guillemette <NinteMars@proton.me>
2024-12-03 15:25:38 -0500

    Add env to allow for region change
    Adds env to app to allow the region to be changed, which is useful for
    changing the region if us-east-1 has too many applications running on
    it.
    
    Signed-off-by: Jacob Guillemette <NinteMars@proton.me>

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import aws_cdk as cdk
 from pointless_analogies.pointless_analogies_stack import PointlessAnalogiesStack
 
 
+
 app = cdk.App()
 PointlessAnalogiesStack(app, "PointlessAnalogiesStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
@@ -15,7 +16,7 @@ PointlessAnalogiesStack(app, "PointlessAnalogiesStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
