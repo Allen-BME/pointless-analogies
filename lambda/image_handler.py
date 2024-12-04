697841102d6cdbe267240fa482e61ddd1e49efba
Jacob Guillemette <NinteMars@proton.me>
2024-12-04 10:49:29 -0500

    Add image hash and categories to dynamoDB table
    Adds inserting the image hash, two categories, and initial vote counts
    of 0 to the image_handler notification lambda.
    
    Signed-off-by: Jacob Guillemette <NinteMars@proton.me>

--- lambda/image_handler.py
+++ lambda/image_handler.py
@@ -1,14 +1,48 @@
 import boto3
 import uuid
+import os
+import json
 
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    client = boto3.client('lambda')
+    TABLE_NAME = os.environ['TABLE_NAME']
+    table = dynamodb.Table(TABLE_NAME)
+
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
         new_key = f"uniq-{uuid.uuid4()}"
+
+        response = client.invoke(
+            FunctionName='get-categories',
+            InvocationType='RequestResponse'
+        )
+
+        response_payload = json.loads(response['Payload'].read().decode('utf-8'))
+        categories: str = response_payload['body'] if 'body' in response_payload else response_payload
+
+        split_categories = categories.split('-')
+
+        category1 = split_categories[0]
+        category2 = split_categories[1]
+
+        table_response = table.put_item(
+            Item = {
+                "ImageHash": new_key,
+                "Category1": category1,
+                "Category2": category2,
+                "Category1Votes": 0,
+                "Category2Votes": 0
+            }
+        )
+
         s3.copy_object(Bucket=bucket, Key=new_key, CopySource=f"{bucket}/{key}")
         s3.delete_object(Bucket=bucket, Key=key)
     
-    return "Image successfully renamed"
+    return {
+        "statusCode": 200,
+        "body": f"Item added to bucket and table. {response}\n\n\n{table_response}"
+    }
