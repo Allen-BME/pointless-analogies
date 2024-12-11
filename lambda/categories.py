06e117c754037e29086f756113a840023d88c48c
Samuel Stanley <samuelstanley0101@gmail.com>
2024-12-11 09:58:05 -0500

    Cleaned up the stack

--- lambda/categories.py
+++ lambda/categories.py
@@ -4,7 +4,7 @@ import json
 import os
 from botocore.exceptions import ClientError
 
-def lambda_handler(event, context):
+def get_categories_function(event, context):
     categories = ["chair", "hat", "boat", "shoe", "wig", "hair tie", "apple", "toothbrush",
               "fork", "shirt", "belt", "table", "bat", "car", "pen", "bicycle",
               "ice cube tray", "knife", "purse", "cat"]
